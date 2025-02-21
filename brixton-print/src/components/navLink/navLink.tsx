cff090d9235ee9fda970d5962c8680206c880a42
ShakiraKP <103694861+ShakiraKP@users.noreply.github.com>
2025-02-21 01:20:36 +0000

    Remove variant prop from navLink

--- brixton-print/src/components/navLink/navLink.tsx
+++ brixton-print/src/components/navLink/navLink.tsx
@@ -5,19 +5,15 @@ import {
   Button,
   Link as ChakraLink,
   LinkProps as ChakraLinkProps,
-  ConditionalValue,
 } from "@chakra-ui/react";
 import { usePathname } from "next/navigation";
 
 type NavLinkProps = ChakraLinkProps & {
   href: string;
   text: string;
-  variant?: ConditionalValue<
-    "outline" | "link" | "solid" | "active" | "ghost" | undefined
-  >;
 };
 
-const NavLink = ({ href, text, variant, children, ...props }: NavLinkProps) => {
+const NavLink = ({ href, text, children, ...props }: NavLinkProps) => {
   const pathname = usePathname();
   const isActive = pathname === href;
 
